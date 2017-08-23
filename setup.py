acebfff04ef654ce0b19082bd51e80de093d6b77
mwtmurphy <mitch@spotlightdata.co.uk>
2017-08-23 11:43:20 +0100

    release 0.1.4 established

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def get_requirements():
     return packages
 
 setup(name="preprocessing",
-      version="0.1.3",
+      version="0.1.4",
       classifiers=["Natural Language :: English",
                    "Programming Language :: Python :: 3",
                    "Programming Language :: Python :: 3.6"],
